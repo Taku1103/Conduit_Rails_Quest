41bdbcb07ce5861cc19f8ff19de9b8116c2b3434
Taku1103 <orzorz1243@gmail.com>
2023-12-23 08:55:32 +0900

    基本CRUD実装

--- README.md
+++ README.md
@@ -27,13 +27,21 @@ Things you may want to cover:
 * indexページのarticleの動的描画
   * 〇dbにサンプル挿入
   * 〇それを描画
-  * タグは一つのみ
-  * 作成日
-  * いいね数はランダム
+    * タグは一つのみ
+    * 作成日
+    * いいね数はランダム
 
 * newページ
   * newページのarticleのcreate
+  * バリデーション
+  * バリデーションの警告表示
 
+* 〇showページ
+* 〇showページに遷移
+  * 〇showページでdelete
+  * 〇showページでeditページ遷移
+  * 〇update
+* new,editとフォーム共通化
 
 * (xやらない)タグは配列にして描画時にイテレートして描画させる
 
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,19 +15,34 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     if @article.save
     flash[:success] = "article created!"
-    # redirect_to index_path
+    redirect_to articles_path
     else
-
+      flash.now[:danger] = "article did Not create"
+      render "articles/new"
     end
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+    if @article.save
+      @article.update(article_params)
+      flash[:success] = "article edited!"
+      redirect_to articles_path
+    else
+      flash.now[:danger] = "article did Not edited"
+      render "articles/edit"
+    end
   end
 
-  def delete
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:success] = "article deleted!"
+    redirect_to articles_path
   end
 
 
